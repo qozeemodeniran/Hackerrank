b2489fc6b05bda06bde1c83328466a9b120cc34b
Qozeem Odeniran <muhdqozeem@gmail.com>
2022-10-13 08:39:15 -0400

    updated main.py

--- python/main.py
+++ python/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm, I love you!')
+    print_hi('PyCharm, you are the best!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
